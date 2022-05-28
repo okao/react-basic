bcdcebb615c0680304d8c78b233c648830943fd2
Hamzath Anees <hamzath.anees@ooredoo.mv>
2022-05-28 09:42:30 +0500

    Start debug

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './styles/index.scss';
 import App from './App';
+import {StrictMode} from 'react';
 
-ReactDOM.render(<App/>, document.querySelector('#root'));
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
